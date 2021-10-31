c13149e595db7a75ed98d7ba31afe8ff070479ff
Gabriel Lantin <mushymanee@gmail.com>
2021-10-30 18:30:17 -0700

    added associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,40 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const PostComment = require('./PostComment');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
 
-// module.exports = {};
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+})
+
+module.exports = { User, Post, Comment, PostComment };
