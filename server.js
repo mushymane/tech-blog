bb78d1c8f1c9a0170abf4c9968cfbbbf5f4493c1
Gabriel Lantin <mushymanee@gmail.com>
2021-10-30 17:20:52 -0700

    initial commit - server.js and dependencies

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const path = require('path');
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+// const helpers = require('./utls/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sesh = {
+    secret = 'ultra secret ooo',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({db: sequelize})
+}
+
+app.use(session(sesh));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
+});
