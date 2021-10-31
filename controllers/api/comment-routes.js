84656fabf25ad273ac0a07df65f54841104ee2c6
Gabriel Lantin <mushymanee@gmail.com>
2021-10-30 20:13:49 -0700

    added comment routes (might need to relocate)

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -9,15 +9,13 @@ const { Post, Comment } = require('../../models');
 // router.post('/', withAuth, async (req, res) => {
 router.post('/', async (req, res) => {
     try {
-        const newComment = await Comment.create({
-            ...req.body,
-            // *** UNCOMMENT WHEN DONE WITH TESTING ***
-            // user_id: req.session.user_id
-        });
+        // const newComment = await Comment.create({
+        //     ...req.body,
+        //     // *** UNCOMMENT WHEN DONE WITH TESTING ***
+        //     // user_id: req.session.user_id
+        // });
 
         const newComment = await Comment.create(req.body);
-
-
 
         res.status(200).json(newComment);
     } catch (err) {
